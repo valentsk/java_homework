94bfe758dfed6fde073abd647b9a067a9190e351
valentsk <skredovavalenti@mail.ru>
2023-01-26 14:44:22 +0300

    Resolve ex01

--- HW001/ex001.java
+++ HW001/ex001.java
@@ -1,14 +1,34 @@
+// Вычислить n-ое треугольного число(сумма чисел от 1 до n), 
+// n! (произведение чисел от 1 до n) тр 5 = 1+2+3+4+5 5! = 1*2*3*4*5
+
 import java.util.Scanner;
 public class ex001 {
+
+    static int CountNug(Integer number) {
+        Integer sum = 0;
+        for (Integer i = 1; i < number + 1; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    
+    static int CountFibo(Integer number) {
+        Integer fibo = 1;
+        for (Integer i = 1; i < number + 1; i++){
+            fibo *= i;
+        }
+        return fibo;
+    }  
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
-        System.out.println("Введите N:");
+        System.out.print("Введите N: ");
         Integer number = iScanner.nextInt();
         iScanner.close();
-        System.out.printf("Для n треугольника: " + number);
-        System.out.println();
-        System.out.printf("Для n!: " + number);
-
+        
+        Integer result_nug = CountNug(number);
+        System.out.println("Сумма чисел от 1 до N: " + result_nug);
+ 
+        Integer result_fibo = CountFibo(number);
+        System.out.println("Произведение чисел от 1 до N: " + result_fibo);
     }        
-    
 }
