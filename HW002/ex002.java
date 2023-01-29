44f88f7a26ecf45c36ffe2e19b4e773d76e1de21
valentsk <skredovavalenti@mail.ru>
2023-01-29 22:06:26 +0300

    No Resolve ex02

--- HW002/ex002.java
+++ HW002/ex002.java
@@ -13,11 +13,55 @@
 // Студент Петрова получил 4 по предмету Информатика.
 // Студент Краснов получил 5 по предмету Физика.
 
-public class ex002 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-    
+public class ex002 {
+
+    static void ParseFile(String text) {
+        StringBuilder sb = new StringBuilder();
+        text = text.replace("[^а-яА-Я]", "");
+        System.out.println(text);
+
+        // String[] text_array = text.split("[^а-яА-Я]");
+        String[] text_array = text.split(" ");
+
+        // for (Object x : text_array)
+        // {
+        //     System.out.print(x + " ");
+        // }
+
 
     }
-}
+
+    public static void main(String[] args) throws IOException {
+
+        String text1 = "{'фамилия':'Иванов','оценка':'5','предмет':'Математика'}\n";
+        String text2 = "{'фамилия':'Петрова','оценка':'4','предмет':'Информатика'}\n";
+        String text3 = "{'фамилия':'Краснов','оценка':'5','предмет':'Физика'}\n";
+
+        FileWriter file = new FileWriter("C:\\temp\\java\\homework\\HW002\\json_ex02.txt");
+        file.write(text1);
+        file.write(text2);
+        file.write(text3);
+        file.close();
+
+
+
+        BufferedReader br = new BufferedReader(new FileReader("C:\\temp\\java\\homework\\HW002\\json_ex02.txt"));
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+        while (line != null) {
+            sb.append(line).append("\n");
+            line = br.readLine();
+        }
+        String fileAsString = sb.toString();
+        System.out.println(fileAsString);
+
+
+        ParseFile(fileAsString);
+    }
+
 }
