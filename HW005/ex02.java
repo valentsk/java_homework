b5a61f96d557f21b767cab541726348dec7666f7
valentsk <skredovavalenti@mail.ru>
2023-02-05 19:28:17 +0300

    Resolve ex02

--- HW005/ex02.java
+++ HW005/ex02.java
@@ -18,6 +18,44 @@
 // Петр Петин
 // Иван Ежов.
 // Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности Имени.
+import java.util.*;
+
 public class ex02 {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        String men =    "Иван Иванов Светлана Петрова Кристина Белова Анна Мусина Анна Крутова " +
+                        "Иван Юрин Петр Лыков Павел Чернов Петр Чернышов Мария Федорова " +
+                        "Марина Светлова Мария Савина Мария Рыкова Марина Лугова " +
+                        "Анна Владимирова Иван Мечников Петр Петин Иван Ежов";
+        String[] list_men = men.split(" ");
+        for (int i = 0; i < list_men.length; i += 2) {
+            if (map.containsKey(list_men[i])) {
+                map.put(list_men[i], map.get(list_men[i]) + 1);
+            }
+            else {
+                map.put(list_men[i], 1);
+            }
+        }
+        
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            String key = pair.getKey();
+            Integer value = pair.getValue();
+            System.out.println(key + ":" + value);
+        }
+        System.out.println("-----------------");
+
+        map.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()) 
+        .forEach(System.out::println);
     
+        // Set<String> keys = map.keySet();
+        // System.out.println("Ключи: " + keys);
+ 
+        // ArrayList<Integer> values = new ArrayList<>(map.values());
+        // System.out.println("Значения: " + values);
+
+
+    }
 }
--- HW005/ex03.java
+++ HW005/ex03.java
@@ -1,5 +1,11 @@
 // На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга. И вывести Доску. 0x00000 0000x00 00x0000
 
+import java.util.*;
+
 public class ex03 {
-    
+    public static void main(String[] args) {
+
+
+        
+    }
 }
